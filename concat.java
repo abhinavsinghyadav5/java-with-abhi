dde81903f0377d8733f5a8c6e2636fc43ffc3d5e
abhinavsinghyadav5 <91362425+abhinavsinghyadav5@users.noreply.github.com>
2022-04-14 11:09:29 +0530

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class concat {
+    public static void main(String args[])
+ {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter first string ");
+     String firststr=sc.next();
+     System.out.println("Enter the second string");
+     String secondstr=sc.next();
+     System.out.println("After concatination of strings");
+     System.out.println(firststr + " "+ secondstr);
+ }   
+}
